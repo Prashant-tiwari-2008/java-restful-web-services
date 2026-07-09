1e37dcd5467d8ce5e277dd6648b8b5a66642f56e
Prashant tiwari <prashanttiwari.vns@gmail.com>
2026-07-09 15:04:21 +0530

    first day first commit

--- src/main/java/com/javaStudy/rest_web_services/restful_web_services/RestfulWebServicesApplication.java
+++ src/main/java/com/javaStudy/rest_web_services/restful_web_services/RestfulWebServicesApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RestfulWebServicesApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hello Prashant");
 		SpringApplication.run(RestfulWebServicesApplication.class, args);
 	}
 
