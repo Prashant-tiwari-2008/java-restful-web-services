8cad7866287f344fb9f36c3c4ad0356875b24a47
Prashant tiwari <prashanttiwari.vns@gmail.com>
2026-07-11 16:01:41 +0530

    Hateos added and not working and i do not understand it as well

--- src/main/java/com/javaStudy/rest_web_services/restful_web_services/user/UserResource.java
+++ src/main/java/com/javaStudy/rest_web_services/restful_web_services/user/UserResource.java
@@ -1,12 +1,17 @@
 package com.javaStudy.rest_web_services.restful_web_services.user;
 
 import jakarta.validation.Valid;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 public class UserResource {
@@ -16,20 +21,37 @@ public class UserResource {
         this.userService = userService;
     }
 
+
     @GetMapping("/users")
-    public List<User> getUsers() {
+    public List<User> retrieveAllUsers() {
         return userService.getUsers();
     }
 
-    @GetMapping("/user/{id}")
-    public User getUser(@PathVariable int id) {
-        User currentUser =  userService.getUser(id);
-        if(currentUser == null) {
-            throw new UserNotFoundException("id : " + id);
-        }
-        System.out.printf("currentUser %s $n ", currentUser);
-        return currentUser;
-    }
+
+//    @GetMapping("/user/{id}")
+//    public User getUser(@PathVariable int id) {
+//        User currentUser =  userService.getUser(id);
+//        if(currentUser == null) {
+//            throw new UserNotFoundException("id : " + id);
+//        }
+//        System.out.printf("currentUser %s $n ", currentUser);
+//        return currentUser;
+//    }
+
+
+//  todo : not working
+//    @GetMapping("/user/{id}")
+//    public EntityModel<User> getUser(@PathVariable int id) {
+//        User currentUser =  userService.getUser(id);
+//        if(currentUser == null) {
+//            throw new UserNotFoundException("id : " + id);
+//        }
+//        EntityModel<User> entityModel = EntityModel.of(currentUser);
+//        WebMvcLinkBuilder link = linkTo(methodOn(this.getClass().retrieveAllUsers()));
+//        entityModel.add(link.withRel("all-users"));
+//        return entityModel;
+//    }
+
 
     @PostMapping("/user")
     public ResponseEntity<User> createUser(@Valid  @RequestBody User user) {
